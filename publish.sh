ad5f0382f7dc5c3df26f2711b0d36e623296ed8f
LEE, Hyun-chang <musclyhc@gmail.com>
2016-06-28 11:36:08 +0900

    test

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ] 
+	then
+	echo "No Commit Message"
+	echo "USAGE: $0 commit_message"
+	exit
+fi
+
+# Push the chages of muscly.github.io.raw
+git add .
+git commit -m $1
+git push
+
+# Build the site
+jekyll build
+
+# Copy the site
+cp -r _site/* ../muscly.github.io/
+
+# Push the site
+pushd .
+cd ../muscly.github.io/
+git add .
+git commit -m $1
+git push
+popd
